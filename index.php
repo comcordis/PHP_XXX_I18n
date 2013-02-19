cfaa0b381d160e05e03adefa8424a2ec75d44d05
vinceMeens <v.w.a.meens@comcordis.com>
2013-02-19 14:13:52 +0100

    Initialized triggers

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ require_once 'XXX_I18n_Currency.php';
 require_once 'XXX_I18n_Translation.php';
 require_once 'XXX_I18n_Localization.php';
 
+XXX_I18n_Translation::initialize();
+XXX_I18n_Localization::initialize();
+
 ?>
